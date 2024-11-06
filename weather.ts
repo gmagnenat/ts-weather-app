84ab993dd5079f777a605b8464f5afa2c692a5c9
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-06 14:40:51 +0100

    Init repository

---
+++ weather.ts
@@ -0,0 +1,19 @@
+const GEOCODE_API_URL = "https://geocode.maps.co/search";
+
+async function main(): Promise<number> {
+  // pnpm run weather LOCATION
+  if (process.argv.length !== 3) {
+    console.error("usage: weather LOCATION");
+    return 1;
+  }
+
+  // get location
+  const location = process.argv[2];
+  // convert location to lat/lon
+  // fetch weather data
+  // display weather data
+
+  return await Promise.resolve(0);
+}
+
+main().catch((err) => console.error(err));
